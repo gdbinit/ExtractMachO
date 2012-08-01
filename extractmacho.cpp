0f5317bf10d27bf9626895d24cd84658554b560d
fG <reverser@put.as>
2012-08-01 23:54:14 +0100

    Move the MH_OBJECT extraction to its own function. Still looks ugly :-]

--- extractmacho.cpp
+++ extractmacho.cpp
@@ -42,6 +42,7 @@
 #define DEBUG 1
 
 uint8_t extract_macho(ea_t address, char *outputFilename);
+uint8_t extract_mhobject(ea_t address, char *outputFilename);
 uint8_t extract_fat(ea_t address, char *outputFilename);
 uint8_t extract_binary(ea_t address, char *outputFilename);
 void add_to_fat_list(ea_t address);
@@ -231,7 +232,13 @@ extract_binary(ea_t address, char *outputFilename)
     uint32 magicValue = get_long(address);
     if (magicValue == MH_MAGIC || magicValue == MH_MAGIC_64)
     {
-       retValue = extract_macho(address, outputFilename);
+        // we just need to read mach_header.filetype so no problem in using the 32bit struct
+        struct mach_header header;
+        get_many_bytes(address, &header, sizeof(struct mach_header));
+        if (header.filetype == MH_OBJECT)
+            retValue = extract_mhobject(address, outputFilename);
+        else
+            retValue = extract_macho(address, outputFilename);
     }
     else if (magicValue == FAT_CIGAM)
     {
@@ -245,19 +252,14 @@ extract_binary(ea_t address, char *outputFilename)
     return retValue;
 }
 
-/*
- * function to extract non-fat binaries, 32 and 64bits
- */
-uint8_t 
-extract_macho(ea_t address, char *outputFilename)
+uint8_t
+extract_mhobject(ea_t address, char *outputFilename)
 {
     uint32 magicValue = get_long(address);
     
     struct mach_header *mach_header = NULL;
     struct mach_header_64 *mach_header64 = NULL;
-    
-    uint8_t targetIsMHObject = 0;
-    
+        
     uint8_t arch = 0;
     if (magicValue == MH_MAGIC)
     {
@@ -276,9 +278,6 @@ extract_macho(ea_t address, char *outputFilename)
             msg("[ERROR] Not a valid mach-o binary at %x\n", address);
             return 1;
         }
-        // MH_OBJECT is a more compact format so we need to treat it differently
-        if (mach_header->filetype == MH_OBJECT)
-            targetIsMHObject = 1;
     }
     else if (magicValue == MH_MAGIC_64)
     {
@@ -297,8 +296,203 @@ extract_macho(ea_t address, char *outputFilename)
             return 1;
         }
         arch = 1;
-        if (mach_header64->filetype == MH_OBJECT)
-            targetIsMHObject = 1;
+    }
+    
+    // open output file
+    FILE *outputFile = qfopen(outputFilename, "wb+");
+    if (outputFile == NULL)
+    {
+        msg("[ERROR] Could not open %s file!\n", outputFilename);
+        return 1;
+    }
+    
+    /*
+     * we need to write 3 distinct blocks of data:
+     * 1) the mach_header
+     * 2) the load commands
+     * 3) the code and data from the LC_SEGMENT/LC_SEGMENT_64 commands
+     */
+    
+    // write the mach_header to the file
+    if (arch)
+        qfwrite(outputFile, mach_header64, sizeof(struct mach_header_64));
+    else
+        qfwrite(outputFile, mach_header, sizeof(struct mach_header));
+    
+    // read the load commands
+    uint32_t ncmds = arch ? mach_header64->ncmds : mach_header->ncmds;
+    uint32_t sizeofcmds = arch ? mach_header64->sizeofcmds : mach_header->sizeofcmds;
+    uint32_t headerSize = arch ? sizeof(struct mach_header_64) : sizeof(struct mach_header);
+    
+    uint8_t *loadcmdsBuffer = NULL;
+    loadcmdsBuffer = (uint8_t*)qalloc(sizeofcmds);
+    
+    get_many_bytes(address + headerSize, loadcmdsBuffer, sizeofcmds);
+    // write all the load commands block to the output file
+    // only LC_SEGMENT commands contain further data
+    qfwrite(outputFile, loadcmdsBuffer, sizeofcmds);
+    
+    // and now process the load commands so we can retrieve code and data
+    struct load_command loadCommand;
+    ea_t cmdsBaseAddress = address + headerSize;    
+    ea_t codeOffset = 0;
+    
+    // read segments so we can write the code and data
+    // only the segment commands have useful information
+    for (uint32_t i = 0; i < ncmds; i++)
+    {
+        get_many_bytes(cmdsBaseAddress, &loadCommand, sizeof(struct load_command));
+        struct segment_command segmentCommand;
+        struct segment_command_64 segmentCommand64;
+        // 32bits targets
+        if (loadCommand.cmd == LC_SEGMENT)
+        {
+            get_many_bytes(cmdsBaseAddress, &segmentCommand, sizeof(struct segment_command));
+            if (strncmp(segmentCommand.segname, "", 16) == 0)
+            {
+                ea_t sectionAddress = cmdsBaseAddress + sizeof(struct segment_command);
+                struct section sectionCommand; 
+                // iterate thru all sections to find the first code offset
+                // FIXME: we need to find the lowest one since the section info can be reordered
+                for (uint32_t x = 0; x < segmentCommand.nsects; x++)
+                {
+                    get_many_bytes(sectionAddress, &sectionCommand, sizeof(struct section));
+                    if (sectionCommand.nreloc > 0)
+                    {
+                        uint32_t size = sectionCommand.nreloc*sizeof(struct relocation_info);
+                        uint8_t *buf = (uint8_t*)qalloc(size);
+                        get_many_bytes(address + sectionCommand.reloff, buf, size);
+                        qfseek(outputFile, sectionCommand.reloff, SEEK_SET);
+                        qfwrite(outputFile, buf, size);
+                        qfree(buf);
+                    }
+                    sectionAddress += sizeof(struct section);
+                }
+                codeOffset = segmentCommand.fileoff;
+            }
+            // for all other segments the fileoffset info in the LC_SEGMENT is valid so we can use it
+            else
+            {
+                codeOffset = segmentCommand.fileoff;
+            }
+            // read and write the data
+            uint8_t *buf = (uint8_t*)qalloc(segmentCommand.filesize);
+            get_many_bytes(address + codeOffset, buf, segmentCommand.filesize);
+            // always set the offset
+            qfseek(outputFile, codeOffset, SEEK_SET);
+            qfwrite(outputFile, buf, segmentCommand.filesize);
+            qfree(buf);
+        }
+        else if (loadCommand.cmd == LC_SYMTAB)
+        {
+            struct symtab_command symtabCommand;
+            get_many_bytes(cmdsBaseAddress, &symtabCommand, sizeof(struct symtab_command));
+            if (symtabCommand.symoff > 0)
+            {
+                void *buf = qalloc(symtabCommand.nsyms*sizeof(struct nlist));
+                get_many_bytes(address + symtabCommand.symoff, buf, symtabCommand.nsyms*sizeof(struct nlist));
+                qfseek(outputFile, symtabCommand.symoff, SEEK_SET);
+                qfwrite(outputFile, buf, symtabCommand.nsyms*sizeof(struct nlist));
+                qfree(buf);
+            }
+            if (symtabCommand.stroff > 0)
+            {
+                void *buf = qalloc(symtabCommand.strsize);
+                get_many_bytes(address + symtabCommand.stroff, buf, symtabCommand.strsize);
+                qfseek(outputFile, symtabCommand.stroff, SEEK_SET);
+                qfwrite(outputFile, buf, symtabCommand.strsize);
+                qfree(buf);
+            }
+        }
+        // 64bits targets
+        else if (loadCommand.cmd == LC_SEGMENT_64)
+        {
+            get_many_bytes(cmdsBaseAddress, &segmentCommand64, sizeof(struct segment_command_64));
+            if(strncmp(segmentCommand64.segname, "__TEXT", 16) == 0)
+            {
+                ea_t sectionAddress = cmdsBaseAddress + sizeof(struct segment_command_64);
+                struct section_64 sectionCommand64;
+                for (uint32_t x = 0; x < segmentCommand64.nsects; x++)
+                {
+                    get_many_bytes(sectionAddress, &sectionCommand64, sizeof(struct section_64));
+                    if (strncmp(sectionCommand64.sectname, "__text", 16) == 0)
+                    {
+                        codeOffset = sectionCommand64.offset;
+                        break;
+                    }
+                    sectionAddress += sizeof(struct section_64);
+                }
+            }
+            else
+            {
+                codeOffset = segmentCommand64.fileoff;
+            }
+            // read and write the data
+            uint8_t *buf = (uint8_t*)qalloc(segmentCommand64.filesize);
+            get_many_bytes(address + codeOffset, buf, segmentCommand64.filesize);
+            qfseek(outputFile, codeOffset, SEEK_SET);
+            qfwrite(outputFile, buf, segmentCommand64.filesize);
+            qfree(buf);
+        }
+        cmdsBaseAddress += loadCommand.cmdsize;
+    }
+    
+    // all done, close file and free remaining buffers!
+    qfclose(outputFile);
+    qfree(mach_header);
+    qfree(mach_header64);
+    qfree(loadcmdsBuffer);
+    return 0;
+
+}
+
+/*
+ * function to extract non-fat binaries, 32 and 64bits
+ */
+uint8_t 
+extract_macho(ea_t address, char *outputFilename)
+{
+    uint32 magicValue = get_long(address);
+    
+    struct mach_header *mach_header = NULL;
+    struct mach_header_64 *mach_header64 = NULL;
+        
+    uint8_t arch = 0;
+    if (magicValue == MH_MAGIC)
+    {
+#if DEBUG
+        msg("[DEBUG] Target is 32bits!\n");
+#endif
+        mach_header = (struct mach_header *)qalloc(sizeof(struct mach_header));
+        // retrieve mach_header contents
+        if(!get_many_bytes(address, mach_header, sizeof(struct mach_header)))
+        {
+            msg("[ERROR] Read bytes failed!\n");
+            return 1;
+        }
+        if (validate_macho((void*)mach_header))
+        {
+            msg("[ERROR] Not a valid mach-o binary at %x\n", address);
+            return 1;
+        }
+    }
+    else if (magicValue == MH_MAGIC_64)
+    {
+#if DEBUG
+        msg("[DEBUG] Target is 64bits!\n");
+#endif
+        mach_header64 = (struct mach_header_64 *)qalloc(sizeof(struct mach_header_64));
+        if(!get_many_bytes(address, mach_header64, sizeof(struct mach_header_64)))
+        {
+            msg("[ERROR] Read bytes failed!\n");
+            return 1;
+        }
+        if (validate_macho((void*)mach_header64))
+        {
+            msg("[ERROR] Not a valid mach-o binary at %x\n", address);
+            return 1;
+        }
+        arch = 1;
     }
     
     // open output file
@@ -370,28 +564,6 @@ extract_macho(ea_t address, char *outputFilename)
                     sectionAddress += sizeof(struct section);
                 }
             }
-            else if (strncmp(segmentCommand.segname, "", 16) == 0 && targetIsMHObject)
-            {
-                ea_t sectionAddress = cmdsBaseAddress + sizeof(struct segment_command);
-                struct section sectionCommand; 
-                // iterate thru all sections to find the first code offset
-                // FIXME: we need to find the lowest one since the section info can be reordered
-                for (uint32_t x = 0; x < segmentCommand.nsects; x++)
-                {
-                    get_many_bytes(sectionAddress, &sectionCommand, sizeof(struct section));
-                    if (sectionCommand.nreloc > 0)
-                    {
-                        uint32_t size = sectionCommand.nreloc*sizeof(struct relocation_info);
-                        uint8_t *buf = (uint8_t*)qalloc(size);
-                        get_many_bytes(address + sectionCommand.reloff, buf, size);
-                        qfseek(outputFile, sectionCommand.reloff, SEEK_SET);
-                        qfwrite(outputFile, buf, size);
-                        qfree(buf);
-                    }
-                    sectionAddress += sizeof(struct section);
-                }
-                codeOffset = segmentCommand.fileoff;
-            }
             // for all other segments the fileoffset info in the LC_SEGMENT is valid so we can use it
             else
             {
@@ -404,27 +576,6 @@ extract_macho(ea_t address, char *outputFilename)
             qfseek(outputFile, codeOffset, SEEK_SET);
             qfwrite(outputFile, buf, segmentCommand.filesize);
             qfree(buf);
-        }
-        else if (loadCommand.cmd == LC_SYMTAB && targetIsMHObject)
-        {
-            struct symtab_command symtabCommand;
-            get_many_bytes(cmdsBaseAddress, &symtabCommand, sizeof(struct symtab_command));
-            if (symtabCommand.symoff > 0)
-            {
-                void *buf = qalloc(symtabCommand.nsyms*sizeof(struct nlist));
-                get_many_bytes(address + symtabCommand.symoff, buf, symtabCommand.nsyms*sizeof(struct nlist));
-                qfseek(outputFile, symtabCommand.symoff, SEEK_SET);
-                qfwrite(outputFile, buf, symtabCommand.nsyms*sizeof(struct nlist));
-                qfree(buf);
-            }
-            if (symtabCommand.stroff > 0)
-            {
-                void *buf = qalloc(symtabCommand.strsize);
-                get_many_bytes(address + symtabCommand.stroff, buf, symtabCommand.strsize);
-                qfseek(outputFile, symtabCommand.stroff, SEEK_SET);
-                qfwrite(outputFile, buf, symtabCommand.strsize);
-                qfree(buf);
-            }
         }
         // 64bits targets
         else if (loadCommand.cmd == LC_SEGMENT_64)
